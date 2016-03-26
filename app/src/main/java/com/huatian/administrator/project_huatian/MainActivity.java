5e82323b416ef69517a4f8794c6afa95f15b8daf
Danielxj <shaonian423@sina.com>
2016-03-26 17:18:13 +0800

    添加了url地址 mainactivity 里添加了点击事件

--- app/src/main/java/com/huatian/administrator/project_huatian/MainActivity.java
+++ app/src/main/java/com/huatian/administrator/project_huatian/MainActivity.java
@@ -1,8 +1,18 @@
 package com.huatian.administrator.project_huatian;
 
+import android.support.v4.app.Fragment;
+import android.widget.RadioGroup;
+
 import com.huatian.administrator.project_huatian.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends BaseActivity {
+
+    private RadioGroup mRg;
+    private List<Fragment> fragmentList;
+
     @Override
     protected int getContentResid() {
         return R.layout.activity_main;
@@ -10,5 +20,26 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void init() {
+
+        fragmentList=new ArrayList<>();
+
+
+        mRg = (RadioGroup) findViewById(R.id.rg_tab);
+        mRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rb_tab_home:
+                        break;
+                    case R.id.rb_tab_discover:
+                        break;
+                    case R.id.rb_tab_personal:
+                        break;
+                }
+            }
+        });
+
+        mRg.getChildAt(0).performClick();
+
     }
 }
