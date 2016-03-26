755f9444a18d03c83bf05196debfd8c6a6e438e3
NO1f <hc1251249582@sina.com>
2016-03-26 17:00:45 +0800

    添加init

---
+++ app/src/main/java/com/huatian/administrator/project_huatian/fragment/Discover_Fragment.java
@@ -0,0 +1,27 @@
+package com.huatian.administrator.project_huatian.fragment;
+
+import android.os.Bundle;
+
+import com.huatian.administrator.project_huatian.base.BaseFragment;
+
+/**
+ * @类描述: ${TODO}
+ * @创建时间：2016/3/26 16:57
+ * @备注：
+ */
+public class Discover_Fragment extends BaseFragment {
+
+    public static Discover_Fragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        Discover_Fragment fragment = new Discover_Fragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    
+    @Override
+    protected int getContentResid() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/huatian/administrator/project_huatian/fragment/Home_Fragment.java
@@ -0,0 +1,27 @@
+package com.huatian.administrator.project_huatian.fragment;
+
+import android.os.Bundle;
+
+import com.huatian.administrator.project_huatian.base.BaseFragment;
+
+/**
+ * @类描述: ${TODO}
+ * @创建时间：2016/3/26 16:57
+ * @备注：
+ */
+public class Home_Fragment extends BaseFragment {
+
+    public static Home_Fragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        Home_Fragment fragment = new Home_Fragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    
+    @Override
+    protected int getContentResid() {
+        return 0;
+    }
+}
